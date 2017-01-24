d7fccf2a9ae4a62ccb778148451251c939802b1c
jglanz <jonathan.glanz@gmail.com>
2017-01-23 20:21:32 -0500

    cleaned up file structure

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 /**
  * All log functions
  */
-import * as Log from './TypeLogger'
+import * as Log from './Logger'
+
+/**
+ * Export all functions from ./log
+ */
+export * from './Logger'
 
 /**
  * The default styler with chalk
  */
 import DefaultStyler from './DefaultStyler'
 
-/**
- * Export all functions from ./log
- */
-export * from './TypeLogger'
 
 /**
  * Shortcut to Log.create
